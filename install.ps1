7b950be2577ca19511d07b82ecf6450018190db3
Max Strange <max.strange@synapse.com>
2018-03-26 16:46:26 -0700

    Add install.ps1

---
+++ install.ps1
@@ -0,0 +1,58 @@
+﻿function install
+{
+    Param ([uri] $gitlink, [String] $dirname)
+    $path = Join-Path C:\Users\maxst\.vim\bundle $dirname
+    If (-Not (Test-Path $path))
+    {
+        Write-Host "Installing $dirname..."
+        git clone $gitlink $path
+    }
+    Set-Location $path
+    vim -c q "helptags doc/"
+}
+
+# Exit the script on cmdlet error
+$ErrorActionPreference = "Stop"
+
+If (-Not (Test-Path C:\Users\maxst\.vim\bundle))
+{
+    Write-Host "Installing Pathogen..."
+    New-Item -Path C:\Users\maxst\.vim\autoload -Type Directory -Force
+    New-Item -Path C:\Users\maxst\.vim\bundle -Type Directory
+    Invoke-WebRequest -Uri https://tpo.pe/pathogen.vim -OutFile C:\Users\maxst\.vim\autoload\pathogen.vim
+}
+
+install https://github.com/kien/rainbow_parentheses.vim.git rainbow_parentheses
+install https://github.com/elixir-lang/vim-elixir.git vim-elixir
+install https://github.com/ctrlpvim/ctrlp.vim.git ctrlp
+install https://github.com/jeetsukumaran/vim-buffergator.git buffergator
+install https://github.com/tpope/vim-fugitive.git vim-fugitive
+install https://github.com/tmhedberg/SimpylFold.git SimplyFold
+install https://github.com/PProvost/vim-ps1.git vim-ps1
+install https://github.com/rust-lang/rust.vim.git rust.vim
+
+If (-Not (Test-Path C:\Users\maxst\.vim\ftdetect))
+{
+    Write-Host "Installing vim-scala..."
+    New-Item -Type Directory -Path C:\Users\maxst\.vim\ftdetect
+    New-Item -Type Directory -Path C:\Users\maxst\.vim\indent
+    New-Item -Type Directory -Path C:\Users\maxst\.vim\syntax
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/ftdetect/scala.vim" -OutFile "C:\Users\maxst\.vim\ftdetect\scala.vim"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/indent/scala.vim" -OutFile  "C:\Users\maxst\.vim\indent\scala.vim"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/syntax/scala.vim" -OutFile "C:\Users\maxst\.vim\syntax\scala.vim"
+}
+
+If (-Not (Test-Path C:\Users\maxst\.vim\bundle\vim-airline))
+{
+    If (-Not (Test-Path C:\Users\maxst\fonts))
+    {
+        Write-Host "Installing airline..."
+        git clone git://github.com/powerline/fonts C:\Users\maxst/fonts
+        Set-Location C:\Users\maxst\fonts
+        .\install.ps1
+    }
+    Set-Location C:\Users\maxst
+    git clone https://github.com/vim-airline/vim-airline C:\Users\maxst\.vim\bundle\vim-airline
+}
+Set-Location C:\Users\maxst\.vim\bundle\vim-airline
+vim -c q "helptags doc/"
