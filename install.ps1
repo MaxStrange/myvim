1c00f988bf0a23044bfdd2aded1fa237cf5c0c51
Max Strange <strangem@microsoft.com>
2023-03-23 15:25:05 -0700

    Update ps install

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 ﻿function install
 {
     Param ([uri] $gitlink, [String] $dirname)
-    $path = Join-Path C:\Users\maxst\vimfiles\bundle $dirname
+    $path = Join-Path C:\Users\strangem\vimfiles\bundle $dirname
     If (-Not (Test-Path $path))
     {
         Write-Host "Installing $dirname..."
@@ -14,12 +14,12 @@
 # Exit the script on cmdlet error
 $ErrorActionPreference = "Stop"
 
-If (-Not (Test-Path C:\Users\maxst\vimfiles\bundle))
+If (-Not (Test-Path C:\Users\strangem\vimfiles\bundle))
 {
     Write-Host "Installing Pathogen..."
-    New-Item -Path C:\Users\maxst\vimfiles\autoload -Type Directory -Force
-    New-Item -Path C:\Users\maxst\vimfiles\bundle -Type Directory
-    Invoke-WebRequest -Uri https://tpo.pe/pathogen.vim -OutFile C:\Users\maxst\vimfiles\autoload\pathogen.vim
+    New-Item -Path C:\Users\strangem\vimfiles\autoload -Type Directory -Force
+    New-Item -Path C:\Users\strangem\vimfiles\bundle -Type Directory
+    Invoke-WebRequest -Uri https://tpo.pe/pathogen.vim -OutFile C:\Users\strangem\vimfiles\autoload\pathogen.vim
 }
 
 install https://github.com/kien/rainbow_parentheses.vim.git rainbow_parentheses
@@ -32,29 +32,29 @@ install https://github.com/PProvost/vim-ps1.git vim-ps1
 install https://github.com/rust-lang/rust.vim.git rust.vim
 install https://github.com/vhda/verilog_systemverilog.vim
 
-If (-Not (Test-Path C:\Users\maxst\vimfiles\ftdetect))
+If (-Not (Test-Path C:\Users\strangem\vimfiles\ftdetect))
 {
     Write-Host "Installing vim-scala..."
-    New-Item -Type Directory -Path C:\Users\maxst\vimfiles\ftdetect
-    New-Item -Type Directory -Path C:\Users\maxst\vimfiles\indent
-    New-Item -Type Directory -Path C:\Users\maxst\vimfiles\syntax
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/ftdetect/scala.vim" -OutFile "C:\Users\maxst\vimfiles\ftdetect\scala.vim"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/indent/scala.vim" -OutFile  "C:\Users\maxst\vimfiles\indent\scala.vim"
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/syntax/scala.vim" -OutFile "C:\Users\maxst\vimfiles\syntax\scala.vim"
+    New-Item -Type Directory -Path C:\Users\strangem\vimfiles\ftdetect
+    New-Item -Type Directory -Path C:\Users\strangem\vimfiles\indent
+    New-Item -Type Directory -Path C:\Users\strangem\vimfiles\syntax
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/ftdetect/scala.vim" -OutFile "C:\Users\strangem\vimfiles\ftdetect\scala.vim"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/indent/scala.vim" -OutFile  "C:\Users\strangem\vimfiles\indent\scala.vim"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/derekwyatt/vim-scala/master/syntax/scala.vim" -OutFile "C:\Users\strangem\vimfiles\syntax\scala.vim"
 }
 
-If (-Not (Test-Path C:\Users\maxst\vimfiles\bundle\vim-airline))
+If (-Not (Test-Path C:\Users\strangem\vimfiles\bundle\vim-airline))
 {
-    If (-Not (Test-Path C:\Users\maxst\fonts))
+    If (-Not (Test-Path C:\Users\strangem\fonts))
     {
         Write-Host "Installing airline..."
-        git clone git://github.com/powerline/fonts C:\Users\maxst\fonts
-        Set-Location C:\Users\maxst\fonts
+        git clone git://github.com/powerline/fonts C:\Users\strangem\fonts
+        Set-Location C:\Users\strangem\fonts
         .\install.ps1
     }
-    Set-Location C:\Users\maxst
-    git clone https://github.com/vim-airline/vim-airline C:\Users\maxst\vimfiles\bundle\vim-airline
+    Set-Location C:\Users\strangem
+    git clone https://github.com/vim-airline/vim-airline C:\Users\strangem\vimfiles\bundle\vim-airline
 }
-Set-Location C:\Users\maxst\vimfiles\bundle\vim-airline
+Set-Location C:\Users\strangem\vimfiles\bundle\vim-airline
 vim -c q "helptags doc/"
 
